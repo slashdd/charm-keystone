1aace029749839d2fc64eaa219bdc7a034bf7624
Ian Wienand <iwienand@redhat.com>
2019-03-24 20:32:58 +0000

    Replace openstack.org git:// URLs with https://
    This is a mechanically generated change to replace openstack.org
    git:// URLs with https:// equivalents.
    
    This is in aid of a planned future move of the git hosting
    infrastructure to a self-hosted instance of gitea (https://gitea.io),
    which does not support the git wire protocol at this stage.
    
    This update should result in no functional change.
    
    For more information see the thread at
    
     http://lists.openstack.org/pipermail/openstack-discuss/2019-March/003825.html
    
    Change-Id: Id716f20756ffa375cb2029d62054cc41c9e11f92

--- unit_tests/test_keystone_hooks.py
+++ unit_tests/test_keystone_hooks.py
@@ -162,10 +162,10 @@ class KeystoneRelationTests(CharmTestCase):
         openstack_origin_git = {
             'repositories': [
                 {'name': 'requirements',
-                 'repository': 'git://git.openstack.org/openstack/requirements',  # noqa
+                 'repository': 'https://git.openstack.org/openstack/requirements',  # noqa
                  'branch': 'stable/juno'},
                 {'name': 'keystone',
-                 'repository': 'git://git.openstack.org/openstack/keystone',
+                 'repository': 'https://git.openstack.org/openstack/keystone',
                  'branch': 'stable/juno'}
             ],
             'directory': '/mnt/openstack-git',
@@ -563,10 +563,10 @@ class KeystoneRelationTests(CharmTestCase):
         openstack_origin_git = {
             'repositories': [
                 {'name': 'requirements',
-                 'repository': 'git://git.openstack.org/openstack/requirements',  # noqa
+                 'repository': 'https://git.openstack.org/openstack/requirements',  # noqa
                  'branch': 'stable/juno'},
                 {'name': 'keystone',
-                 'repository': 'git://git.openstack.org/openstack/keystone',
+                 'repository': 'https://git.openstack.org/openstack/keystone',
                  'branch': 'stable/juno'}
             ],
             'directory': '/mnt/openstack-git',
